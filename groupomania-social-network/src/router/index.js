4a9de6c667ac538c47024aa48f6ae7dc25c1d536
Anthony Bigotte <bigotte.anthony@gmail.com>
2021-10-19 15:51:07 +0200

    cryptage mot de passe + authentication jwt

--- groupomania-social-network/src/router/index.js
+++ groupomania-social-network/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Profile from '../views/Profile.vue'
+import Signup from "@/views/Signup";
+import CreatePost from "@/views/CreatePost";
 
 const routes = [
   {
@@ -12,6 +15,21 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/createpost',
+    name: 'createpost',
+    component: CreatePost
   }
 ]
 
--- groupomania-social-network/src/store/index.js
+++ groupomania-social-network/src/store/index.js
@@ -1,12 +1,36 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex';
+import axios from 'axios';
+import router from "@/router";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+
+const store = createStore({
+    state: {
+        isLoggedIn: false,
+        jwt: ''
+    },
+    mutations: {
+        logIn: function () {
+            this.$store.state.isLoggedIn = true
+        }
+    },
+    actions: {
+        createAccount: ({commit}, userInfos) => {
+            console.log(commit);
+
+            axios.post('http://127.0.0.1:8000/user/signup', userInfos)
+                .then(res => console.log(res))
+                .catch(err => console.log(err));
+            router.push({path: 'login'});
+        },
+        login: ({commit}, userInfos) => {
+            console.log(commit);
+
+            axios.post('http://127.0.0.1:8000/user/login', userInfos)
+                .then(res => console.log(res))
+                .catch(err => console.log(err));
+        }
+    },
+    modules: {}
 })
+
+export default store;
